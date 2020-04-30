390f40cccb9be0ad8fd6313c0d6ec5065e0cd043
Martink95 <martinkanestrom95@gmail.com>
2020-04-30 02:05:54 +0100

    Initialized project & Retrieved some data from SpaceX api

---
+++ js/launches.js
@@ -0,0 +1,41 @@
+
+let queryString = `https://api.spacexdata.com/v3/launches`;
+
+function getAllLaunches() {
+    fetch(queryString).then((response) => {
+        return response.json();
+    }).then((data) => {
+
+        for(i = 0; i < 10; i++) {
+            let launch_details = data[i].details;
+            let launch_flight_number = data[i].flight_number;
+            let launch_date_year = data[i].launch_year;
+            let launch_date_local = data[i].launch_date_local;
+            let launch_success = data[i].launch_success;
+            let launch_mission_name = data[i].mission_name;
+            let launch_rocket_id = data[i].rocket.rocket_id;
+            let launch_rocket_name = data[i].rocket.rocket_name;
+            let launch_rocket_type = data[i].rocket.rocket_type;
+    
+            console.log(`-----Mission id ${i}-----`);
+            
+            console.log("details: " + launch_details);
+            console.log("flight number: " + launch_flight_number);
+            console.log("year: " + launch_date_year);
+            console.log("date: " + launch_date_local);
+            console.log("success: " + launch_success);
+            console.log("Mission name: " + launch_mission_name);
+            console.log("rocket id: " + launch_rocket_id);
+            console.log("rocket name: " + launch_rocket_name);
+            console.log("rocket type: " + launch_rocket_type);
+            
+        }
+
+        
+        
+        
+        
+    });
+}
+
+getAllLaunches();
